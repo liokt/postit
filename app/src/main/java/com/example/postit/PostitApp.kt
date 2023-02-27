230ae720f74f31e261bc48a3aee05a284e02f394
Lionel Emiliano Mazza <liomazza@MacBook-Pro-de-Lionel.local>
2023-02-27 08:28:10 -0300

    implement hilt di

---
+++ app/src/main/java/com/example/postit/PostitApp.kt
@@ -0,0 +1,8 @@
+package com.example.postit
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PostitApp: Application() {
+}
---
+++ app/src/main/java/com/example/postit/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.example.postit.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.postit.feature_note.data.data_source.NoteDatabase
+import com.example.postit.feature_note.data.repository.NoteRepositoryImpl
+import com.example.postit.feature_note.domain.repository.NoteRepository
+import com.example.postit.feature_note.domain.use_case.DeleteNoteUseCase
+import com.example.postit.feature_note.domain.use_case.GetNotesUseCase
+import com.example.postit.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun providesNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCase(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotesUseCase(repository),
+            deleteNote = DeleteNoteUseCase(repository)
+        )
+    }
+}
