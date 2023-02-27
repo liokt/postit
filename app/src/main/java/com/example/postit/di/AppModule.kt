27057d30de9877229929692a1d4069a5868e2263
Lionel Emiliano Mazza <liomazza@MacBook-Pro-de-Lionel.local>
2023-02-27 09:32:11 -0300

    notes viewmodel

--- app/src/main/java/com/example/postit/di/AppModule.kt
+++ app/src/main/java/com/example/postit/di/AppModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import com.example.postit.feature_note.data.data_source.NoteDatabase
 import com.example.postit.feature_note.data.repository.NoteRepositoryImpl
 import com.example.postit.feature_note.domain.repository.NoteRepository
+import com.example.postit.feature_note.domain.use_case.AddNoteUseCase
 import com.example.postit.feature_note.domain.use_case.DeleteNoteUseCase
 import com.example.postit.feature_note.domain.use_case.GetNotesUseCase
 import com.example.postit.feature_note.domain.use_case.NoteUseCases
@@ -39,7 +40,8 @@ class AppModule {
     fun provideNoteUseCase(repository: NoteRepository): NoteUseCases {
         return NoteUseCases(
             getNotes = GetNotesUseCase(repository),
-            deleteNote = DeleteNoteUseCase(repository)
+            deleteNote = DeleteNoteUseCase(repository),
+            addNote = AddNoteUseCase(repository)
         )
     }
 }
